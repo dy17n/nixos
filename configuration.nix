613360abd8a55fd130ab7f478cf66eb9905eb04c
dy17n <krecker@keemail.me>
2024-12-16 23:49:56 +0000

    flakes

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
