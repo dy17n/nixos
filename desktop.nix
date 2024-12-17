3c5d4b656276adfe9188babbfec50ac27f633747
dy17n <krecker@keemail.me>
2024-12-17 02:03:30 -0600

    woah

--- desktop.nix
+++ desktop.nix
@@ -1,11 +1,17 @@
 {config, pkgs, ...}: {
   home-manager.users.dylan = {
     home.stateVersion = "24.11";
-    home.file."/.config/wayfire.ini".source = ./wayfire.ini;
     home.packages = with pkgs; [
       yofi
       kitty
     ]; 
+
+    services.xserver.displayManager.gdm.enable = true;
+    programs.wayland.enable = true;
+    programs.wayland.plugins = [];
+    home.file."/.config/wayfire.ini".source = ./wayfire.ini;
+
+    programs.firefox.enable = true;
   };
   
 }
