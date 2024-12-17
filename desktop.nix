d41ebc1dd1ec68bc50d4022b0ff8533d2e97b367
dy17n <krecker@keemail.me>
2024-12-17 16:12:50 -0600

    sddm

--- desktop.nix
+++ desktop.nix
@@ -5,13 +5,12 @@
       yofi
       kitty
     ]; 
+  }
 
-    services.xserver.displayManager.gdm.enable = true;
-    programs.wayland.enable = true;
-    programs.wayland.plugins = [];
-    home.file."/.config/wayfire.ini".source = ./wayfire.ini;
+  services.xserver.displayManager.sddm.enable = true;
+  programs.wayfire.enable = true;
+  programs.wayfire.plugins = [];
+  home.file."/.config/wayfire.ini".source = ./wayfire.ini;
 
-    programs.firefox.enable = true;
-  };
-  
+  programs.firefox.enable = true;
 }
